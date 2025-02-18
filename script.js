e97bad655e4c5a1396ac48ae9734e10baa0a84c7
Shrish208 <156112441+Shrish208@users.noreply.github.com>
2025-02-18 15:13:49 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Show popup on page load
+window.onload = function() {
+    document.getElementById("popup").style.display = "flex";
+};
+
+// Close popup
+function closePopup() {
+    document.getElementById("popup").style.display = "none";
+}
